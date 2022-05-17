0b922bc58fb34133826ad9876959469bb041fd8f
lucaspalomodevelop <lucas.palomo@t-online.de>
2022-05-17 17:53:35 +0200

    rename willcome in welcome

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use("/static", express.static(path.join(__dirname, "..", "site")));
 
 app.get("/", function (req, res) {
   res.sendFile(
-    path.join(__dirname, "..", "site", "html", "willcome", "index.html")
+    path.join(__dirname, "..", "site", "html", "welcome", "index.html")
   );
 });
 
